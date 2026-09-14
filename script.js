2a854ab1330e79145cda2409565f6ef31473fb96
Gaurav3623 <yadavgaurav19876@gmail.com>
2026-09-14 19:02:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch("https://www.apicountries.com/countries")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log("Error:", error);
+  });
