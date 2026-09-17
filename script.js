d8d8ceeae0c5c53b6f75aeb4f7ba8e7e2a71884e
Gaurav3623 <yadavgaurav19876@gmail.com>
2026-09-17 16:26:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-fetch("https://www.apicountries.com/countries")
-  .then(response => response.json())
-  .then(data => {
+const countriesContainer = document.querySelector('.countries-container');
+
+fetch('https://countries.dev/countries')
+  .then((res) => res.json())
+  .then((data) => {
     console.log(data);
+
+    data.forEach((country) => {
+      console.log(country.flags.svg);
+
+      const countryCard = document.createElement('a');
+      countryCard.classList.add('country-card');
+      countryCard.href = `details.html?name=${country.name}`;
+
+      countryCard.innerHTML = `
+        <img src="${country.flags.svg}" alt="${country.name} flag">
+        <div class="card-text">
+          <h3>${country.name}</h3>
+        </div>
+      `;
+
+      countriesContainer.append(countryCard);
+    });
   })
-  .catch(error => {
-    console.log("Error:", error);
+  .catch((error) => {
+    console.error('Error fetching countries:', error);
   });
+
